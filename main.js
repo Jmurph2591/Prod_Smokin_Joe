af9c197964cc438406bfe26c94355d0064ef7374
Joe Murphy <jmurphy2591@gmail.com>
2020-04-03 20:39:43 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+/*
+ <script>
+    $(document).ready(function () {
+      $('#btn1').click(function () {
+        $('#box').show() //.animate({width: '350px'});
+      });
+    });
+    $('#btn2').click(function () {
+      /*$('#box').animate({width: '0'},1000, () => {
+        $('#box').hide();
+      });
+      $('#box').hide('slide')
+    });
+  </script>
+  */
+
+  /*
+
+function sideBarOpenClose() {
+    $('#btn1').on('click', '.sidebar', function(e) {
+        e.preventDefault();
+        $('#box').show();
+    })
+}
+*/
+
+
+
