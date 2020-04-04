4b6746c8c09fb6f569de81c153137d57eba7c9e4
Joe Murphy <jmurphy2591@gmail.com>
2020-04-04 14:13:37 -0400

    style updates v2

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
-/*
- <script>
+
+
     $(document).ready(function () {
       $('#btn1').click(function () {
         $('#box').show() //.animate({width: '350px'});
       });
     });
     $('#btn2').click(function () {
-      /*$('#box').animate({width: '0'},1000, () => {
+      $('#box').animate({width: '0'},1000, () => {
         $('#box').hide();
       });
       $('#box').hide('slide')
     });
-  </script>
-  */
+  
 
-  /*
-
+  
+/*
 function sideBarOpenClose() {
     $('#btn1').on('click', '.sidebar', function(e) {
         e.preventDefault();
@@ -23,6 +22,7 @@ function sideBarOpenClose() {
     })
 }
 */
+
 
 
 
