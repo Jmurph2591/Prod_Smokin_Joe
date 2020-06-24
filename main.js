a9753a0e73e64be8c0b484f1a86ecc56ebc7dc42
Joe Murphy <jmurphy2591@gmail.com>
2020-06-23 22:26:24 -0400

    site update

--- main.js
+++ main.js
@@ -1,78 +1,41 @@
-//Open/Close Sidebar
-    $(document).ready(function () {
-      $('#btn1').click(function () {
-        $('#box').show() //.animate({width: '350px'});
-      });
-    });
-    $('#btn2').click(function () {
-      $('#box').animate({width: '0'},1000, () => {
-        $('#box').hide();
-      });
-      $('#box').hide('slide')
-    });
-  
+//open close sidebar
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
 
-  
-// 2nd attempt at sidebar open/close
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
+
+
+//scroll to top button
+const scrollToTop = $('#scrollToTop');
+
+$(function () {
+
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 20) {
+      $('#scrollToTop').fadeIn();
+    } else {
+      $('#scrollToTop').fadeOut();
+    }
+  });
+
+  $('#scrollToTop').click(function () {
+    $('html').animate({ scrollTop: '0' }, 100);
+  });
+});
+
+
+
 /*
-function sideBarOpenClose() {
-    $('#btn1').on('click', '.sidebar', function(e) {
-        e.preventDefault();
-        $('#box').show();
-    })
-}
+//open close sidebar
+$(function() {
+$('#hamburger-btn').on('click', function(){
+  $('#sidebar').animate({width: 'toggle'}, 500);
+});
+$('#closeSidebar').on('click', function(){
+  $('#sidebar').animate({width: 'toggle'});
+});
+});
 */
-
-
-
-//Producer Tips Slider
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {
-    slideIndex = 1
-  }
-  if (n < 1) {
-    slideIndex = x.length
-  }
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndex - 1].style.display = "block";
-}
-
-
-//Scroll to top button
-
-//Get the button
-var mybutton = document.getElementById("topBtn");
-
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction()
-};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-
-
-
-
